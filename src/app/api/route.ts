b25e3bba707681b79613ebe1c11de0b3cc5cd575
JayJay1024 <lygm1062@gmail.com>
2024-04-10 18:23:14 +0800

    Fix response type

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ export async function GET(request: Request) {
   const timeout = Number(new URL(request.url).searchParams.get("timeout") || "NaN");
 
   if (!Number.isNaN(timeout)) {
-    return new Promise((resolve) => {
+    return new Promise<Response>((resolve) => {
       setTimeout(() => {
         resolve(Response.json({ timeout: timeout }));
       }, timeout);
